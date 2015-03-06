daf0ee6cca5c211e718e5db6ee6498d9011b7a6f
nuhuxi <mark.keckeis@gmail.com>
2015-03-05 21:32:57 -0700

    changes made per his instructions on github

--- MultiVision/MultiVision.js
+++ MultiVision/MultiVision.js
@@ -1,7 +1,7 @@
 var express = require("express"),
     stylus = require('stylus'), 
     logger = require('morgan'),
-    bodyparser = require('body-parser');
+    bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -15,7 +15,12 @@ function compile(str, path){
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
-app.use(bodyparser());
+
+//The following two lines added per his instructions on github
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//app.use(bodyparser());
 app.use(stylus.middleware(
   {
     src: __dirname + '/public', 
@@ -24,6 +29,11 @@ app.use(stylus.middleware(
 ));
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/partials/:partialPath', function(req, res){
+  res.render('partials/' + req.params.partialPath);
+
+});
 
 app.get('*', function(req, res){
   res.render('index');
